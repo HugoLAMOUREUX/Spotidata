5529110a68e5dd07009b176581d729409a5c072b
shubhom srivastava <shhbomsrivastav@gmail.com>
2023-04-14 08:38:28 +0530

    updated playlist route

--- back/routes/playlistRoute.js
+++ back/routes/playlistRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getPlaylistTracks } = require("../controllers/playlistController");
+const { getPlaylistDetails } = require("../controllers/playlistController");
 const { getTopTrends } = require("../controllers/playlistController");
 const { getUserPlaylists } = require("../controllers/playlistController");
 
